d01d77e6f028643a802e1f164ccb0eaceabb8b12
ABHAY <abhaybalakrishnan884@gmail.com>
2025-03-08 10:17:59 +0530

    changed port

--- backend/src/app.cjs
+++ backend/src/app.cjs
@@ -8,7 +8,7 @@ const mortuaryRoutes = require('./routes/mortuaryRoutes.cjs');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.BACKEND_PORT || 5000;
 
 app.use(cors({
   origin: true,
